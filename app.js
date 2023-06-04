101277db59031c445e06681489f139abf8b7c5c1
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-06-04 14:54:39 +0530

    Final Refactoring

--- app.js
+++ app.js
@@ -1,51 +1,80 @@
-const p1Button=document.querySelector('#p1Button');
-const p2Button=document.querySelector('#p2Button');
-const p1Display=document.querySelector('#p1Display');
-const p2Display=document.querySelector('#p2Display');
+const p1={
+    score:0,
+    button:document.querySelector('#p1Button'),
+    display:document.querySelector('#p1Display'),
+    wins:0,
+    num:1
+}
+const p2={
+    score:0,
+    button:document.querySelector('#p2Button'),
+    display:document.querySelector('#p2Display'),
+    wins:0,
+    num:2
+}
 const resetButton=document.querySelector('#reset');
 const winningScoreSelect=document.querySelector('#playto');
-let p1Score=0;
-let p2Score=0;
 let winningScore=3;
 let isGameOver=false;
-p1Button.addEventListener('click',function(){
+const winBoard=document.querySelector('.subtitle')
+let isHattrick=false;
+function updatesScores(player,opponent)
+{
     if(!isGameOver)
     {
-        p1Score+=1;
-        if(p1Score===winningScore)
+        player.score+=1;
+        if(player.score===winningScore)
         {
             isGameOver=true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('looser');
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled=true;
+            opponent.button.disabled=true;
+            player.wins+=1;
+            if(player.wins===3)
+            {
+                winBoard.textContent=`Yayy!! Player ${player.num} made a hattrick!`
+                winBoard.classList.add('has-text-warning')
+                isHattrick=true;
+            }
         }
-        p1Display.textContent=p1Score; 
+        player.display.textContent=player.score; 
     }
+}
+p1.button.addEventListener('click',function(){
+    updatesScores(p1,p2);
 });
 
-p2Button.addEventListener('click',function(){
-    if(!isGameOver)
-    {
-        p2Score+=1;
-        if(p2Score===winningScore)
-        {
-            isGameOver=true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('looser');
-        }
-        p2Display.textContent=p2Score; 
-    }
+p2.button.addEventListener('click',function(){
+    updatesScores(p2,p1);
 });
 
 resetButton.addEventListener('click',reset );
 
 function reset(){
     isGameOver=false;
-    p1Score=0;
-    p2Score=0;
-    p1Display.textContent=p1Score;
-    p2Display.textContent=p2Score;
-    p1Display.classList.remove('winner','looser');
-    p2Display.classList.remove('winner','looser');
+    for( p of [p1,p2])
+    {
+        p.score=0;
+        p.display.textContent=p.score;
+        p.display.classList.remove('has-text-success','has-text-danger');
+        p.button.disabled=false;
+        if(isHattrick)
+        {
+            p.wins=0;
+            winBoard.classList.remove('has-text-warning');
+            winBoard.textContent='Use the buttons below to manage the score.';
+            
+        }
+        
+    } 
+
+    if(isHattrick)
+    {
+        isHattrick=false;
+    }
+    
+    
 }
 
 
